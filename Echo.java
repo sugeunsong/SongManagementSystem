f9da5d259e2b8d1d95b51e87edffe2b6d2ea383b
sugeunsong <62562024+sugeunsong@users.noreply.github.com>
2020-03-24 14:35:19 +0900

    Create Echo.java

---
+++ Echo.java
@@ -0,0 +1,13 @@
+
+public class Echo {
+
+	public static void main(String[] args) {
+		System.out.println("Hi java!"); 
+		System.out.print("My name is" + " sugeun.");
+		System.out.println(" I'm student");
+		System.out.printf("age : %d \n", 23);
+		System.out.println("Nice to meet you");
+		
+	}
+
+}
