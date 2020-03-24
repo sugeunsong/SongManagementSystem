e18e7ab5a41f749ce06b4c2550e566515cc299ff
sugeunsong <62562024+sugeunsong@users.noreply.github.com>
2020-03-24 12:20:13 +0900

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,11 +3,11 @@ public class Echo {
     public static void main(String[] args) {
     	System.out.println(" ### Song Management Menu ###");
     	System.out.println(" 1. Add Song");
-		System.out.println(" 2. Delate Song");
-		System.out.println(" 3. Edit Song");
-		System.out.println(" 4. View Song");
-		System.out.println(" 5. Favorite Song");
-		System.out.println(" 6. Exit");
+        System.out.println(" 2. Delate Song");
+	System.out.println(" 3. Edit Song");
+	System.out.println(" 4. View Song");
+	System.out.println(" 5. Favorite Song");
+	System.out.println(" 6. Exit");
 		
 	}
 
