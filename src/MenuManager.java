4752076b227655652714645a296f08e7ba1bfc9d
sugeunsong <62562024+sugeunsong@users.noreply.github.com>
2020-03-24 13:08:03 +0900

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,5 +1,5 @@
 
-public class Echo {
+public class MenuManager {
     public static void main(String[] args) {
     	System.out.println(" ### Song Management Menu ###");
     	System.out.println(" 1. Add Song");
