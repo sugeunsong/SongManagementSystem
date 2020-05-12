7621b06c01ade4ef1ebc8f5d88fa879e87da3b44
sugeunsong <62562024+sugeunsong@users.noreply.github.com>
2020-05-12 15:53:08 +0900

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -6,30 +6,35 @@ public class MenuManager {
 		SongManager songManager = new SongManager(input);
 		int num = -1;
 		while (num != 5) {
-			System.out.println(" ### Song Management Menu ### ");
-			System.out.println(" 1. Add Song");
-			System.out.println(" 2. Delate Song");
-			System.out.println(" 3. Edit Song");
-			System.out.println(" 4. View Songs");
-			System.out.println(" 5. Exit");
-			System.out.println("Select one number between 1 - 5:");
+			showMenu();
 			num = input.nextInt();
-			if(num == 1) {
+			switch(num) {
+			case 1:
 				songManager.addSong();
-			}
-			else if(num == 2) {
+				break;
+			case 2:
 				songManager.delateSong();
-			}
-			else if(num == 3) {
+				break;
+			case 3:
 				songManager.editSong();
-			}
-			else if(num == 4) {
+				break;
+			case 4:
 				songManager.viewSongs();
-			}
-			else {
+				break;
+			default: 
 				continue;
-			}
+			}		
 		} 
+	}
+	
+	public static void showMenu() {
+		System.out.println(" ### Song Management Menu ### ");
+		System.out.println(" 1. Add Song");
+		System.out.println(" 2. Delate Song");
+		System.out.println(" 3. Edit Song");
+		System.out.println(" 4. View Songs");
+		System.out.println(" 5. Exit");
+		System.out.println("Select one number between 1 - 5:");
 	}
 	
 } 
