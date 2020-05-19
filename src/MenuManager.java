32326a1262a476eac8cebcc1e3b18cc2a7ba607c
sugeunsong <62562024+sugeunsong@users.noreply.github.com>
2020-05-19 16:41:14 +0900

    8

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,32 +1,48 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuManager {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		SongManager songManager = new SongManager(input);
+
+		selectMenu(input, songManager);
+
+	}
+
+	public static void selectMenu(Scanner input, SongManager songManager) {
 		int num = -1;
 		while (num != 5) {
-			showMenu();
-			num = input.nextInt();
-			switch(num) {
-			case 1:
-				songManager.addSong();
-				break;
-			case 2:
-				songManager.delateSong();
-				break;
-			case 3:
-				songManager.editSong();
-				break;
-			case 4:
-				songManager.viewSongs();
-				break;
-			default: 
-				continue;
-			}		
+			try {
+				showMenu();
+				num = input.nextInt();
+				switch(num) {
+				case 1:
+					songManager.addSong();
+					break;
+				case 2:
+					songManager.delateSong();
+					break;
+				case 3:
+					songManager.editSong();
+					break;
+				case 4:
+					songManager.viewSongs();
+					break;
+				default: 
+					continue;
+				}
+			}
+			catch(InputMismatchException e) {
+				System.out.println("Please put an integer between 1 and 5!");
+				if (input.hasNext()) {
+					input.next();
+				}
+				num = -1;
+			}
 		} 
 	}
-	
+
 	public static void showMenu() {
 		System.out.println(" ### Song Management Menu ### ");
 		System.out.println(" 1. Add Song");
@@ -36,5 +52,5 @@ public class MenuManager {
 		System.out.println(" 5. Exit");
 		System.out.println("Select one number between 1 - 5:");
 	}
-	
-} 
+
+}
